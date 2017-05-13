dd932135ad7c67e145aa935ce842fc1ea45c02e0
Jamie Barton <hi_jamie@me.com>
2017-05-13 19:19:42 +0100

    Added middleware for token

--- src/index.js
+++ src/index.js
@@ -12,6 +12,19 @@ const networkInterface = createNetworkInterface({
   uri: process.env.REACT_APP_GRAPHQL_URI
 })
 
+networkInterface.use([{
+  applyMiddleware(req, next) {
+    let token = localStorage.getItem('token')
+
+    req.options.headers = {
+      authorization: token ? `Bearer ${token}` : null,
+      ...req.options.headers
+    }
+
+    next()
+  }
+}])
+
 const client = new ApolloClient({
   networkInterface,
   dataIdFromObject: o => o.id,
