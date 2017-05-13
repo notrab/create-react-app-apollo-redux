2a2ab0cd9ab919aa8f9db73fd3ec22db93472894
Jamie Barton <hi_jamie@me.com>
2017-05-13 19:36:52 +0100

    Added enzyme and basic test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
+// @flow
+
 import React from 'react'
 
-const App = props => <div><h1>create-react-app-apollo</h1></div>
+const App = (props: any) => <div><h1>create-react-app-apollo</h1></div>
 
 export default App
